c5a4e80415d4a5b51a7b2d89c63593f3c6e5ed45
FearInTheDark <147612664+FearInTheDark@users.noreply.github.com>
2025-04-23 04:59:38 +0700

    Fixed some bugs

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -1,33 +1,36 @@
 import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { Request } from 'express';
-import * as process from 'node:process';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-	constructor(private readonly jwtService: JwtService) {
-	}
-	
-	async canActivate(context: ExecutionContext): Promise<boolean> {
-		const request = context.switchToHttp().getRequest()
-		const token = this.extractTokenFromHeader(request)
-		console.log(`token: ${token}`)
-		if (!token) throw new UnauthorizedException("No token provided")
-		
-		try {
-			request['user'] = await this.jwtService.verifyAsync(
-				token, {secret: process.env.JWT_SECRET}
-			)
-		} catch (error) {
-			throw new UnauthorizedException(`${error}`)
-		}
-		
-		return true
-	}
-	
-	private extractTokenFromHeader(request: Request): string | undefined {
-		const [type, token] = request.headers.authorization?.split(' ') ?? []
-		console.log(`type: ${type}`)
-		return type === 'Bearer' ? token : undefined
-	}
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly config: ConfigService,
+  ) {
+  }
+  
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const token = this.extractTokenFromHeader(request);
+    console.log(`token: ${token}`);
+    if (!token) throw new UnauthorizedException('No token provided');
+    
+    try {
+      request['user'] = await this.jwtService.verifyAsync(
+        token, { secret: this.config.get<string>('JWT_SECRET') },
+      );
+    } catch (error) {
+      throw new UnauthorizedException(`${error}`);
+    }
+    
+    return true;
+  }
+  
+  private extractTokenFromHeader(request: Request): string | undefined {
+    const [type, token] = request.headers.authorization?.split(' ') ?? [];
+    console.log(`type: ${type}`);
+    return type === 'Bearer' ? token : undefined;
+  }
 }
